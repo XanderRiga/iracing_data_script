4498e24ea036cc0932d5e00298305b156d9cdbdd
Xander Riga <xanderriga@live.com>
2021-04-03 10:34:41 -0700

    Update the way we print that each step is finished

--- main.py
+++ main.py
@@ -36,13 +36,17 @@ async def main():
   top_n_road = top_n_from_results(TOP_N_RESULTS, road_results_list)
   road_subsession_results = await event_results_to_subsession_results(client, top_n_road)
   road_complete_time = datetime.now()
-  build_csv(f'top_road_{road_complete_time}.csv', road_subsession_results)
+  road_csv_name = f'top_road_{road_complete_time}.csv'
+  build_csv(road_csv_name, road_subsession_results)
+  print(f'Road CSV Built at: {road_csv_name}')
 
   oval_results_list = await oval_results(client, driver_ids)
   top_n_oval = top_n_from_results(TOP_N_RESULTS, oval_results_list)
   oval_subsession_results = await event_results_to_subsession_results(client, top_n_oval)
   oval_complete_time = datetime.now()
-  build_csv(f'top_oval_{oval_complete_time}.csv', oval_subsession_results)
+  oval_csv_name = f'top_oval_{oval_complete_time}.csv'
+  build_csv(oval_csv_name, oval_subsession_results)
+  print(f'Oval CSV Built at: {oval_csv_name}')
 
   end = timer()
   print(f'Finished! Script took {end - start} seconds to complete')
